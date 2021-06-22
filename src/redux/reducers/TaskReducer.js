fa66a8f6f1066d1b3b0c96b02968c3621dc82214
Liem <leliem28@gmail.com>
2021-06-22 20:47:05 +0700

    Add TaskReducer

---
+++ src/redux/reducers/TaskReducer.js
@@ -0,0 +1,32 @@
+const stateDefault = {
+    arrTask: [
+        {taskName: ''}
+    ]
+}
+
+export const TaskReducer = (state = stateDefault, action) => {
+    switch (action.type) {
+        case 'ADD_TASK': {
+
+            return {...state}
+        }
+            
+        case 'MODIFY_TASK': {
+            
+            return {...state}
+        }
+
+        case 'DELETE_TASK': {
+            
+            return {...state}
+        }
+
+        case 'COMPLETE_TASK': {
+            
+            return {...state}
+        }
+    
+        default: return state;
+           
+    }
+}
